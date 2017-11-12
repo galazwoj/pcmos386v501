9b6ddb20b2aad17f209e236d1a815758b549c467
Wojciech Galazka <wojciech.galazka@gmail.com>
2017-11-12 12:27:32 +0100

    $$mos.sys and $$eval sys finally compile

--- SOURCES/vars.bat
+++ SOURCES/vars.bat
@@ -1,6 +1,6 @@
 cd \msc51\bin
 call msc51
 set path=\pcmos386\bin;\masm51\bin;%path%
-cd \pcmos386
-set LIB=D:\pcmos386\sources\libs;%LIB%
-set INCLUDE=D:\pcmos386\sources\INCLUDE;%INCLUDE%
+cd \pcmos386\sources
+set LIB=\pcmos386\sources\libs;%LIB%
+set INCLUDE=\pcmos386\sources\INCLUDE;%INCLUDE%
